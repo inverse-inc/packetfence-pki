d3de61d4fbc0bea2572206dce8fd04afbe00949e
nqb <nqb@users.noreply.github.com>
2018-06-27 13:42:20 -0400

    fixes #8

--- pki/forms.py
+++ pki/forms.py
@@ -19,9 +19,9 @@ class UserCreateForm(UserCreationForm):
     # declare the fields you will show
     username = forms.CharField(label="Your Username")
     # first password field
-    password1 = forms.CharField(label="Your Password")
+    password1 = forms.CharField(label="Your Password", widget=forms.PasswordInput)
     # confirm password field
-    password2 = forms.CharField(label="Repeat Your Password")
+    password2 = forms.CharField(label="Repeat Your Password", widget=forms.PasswordInput)
     email = forms.EmailField(label = "Email Address")
     first_name = forms.CharField(label = "Name")
     last_name = forms.CharField(label = "Surname")
